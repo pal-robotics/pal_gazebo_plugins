66a222112b4cd31a0efc0f1a6acf4acd4cb65dcf
Victor Lopez <victor.lopez@pal-robotics.com>
2019-05-14 11:59:45 +0200

    Update PAL licenses

--- include/pal_gazebo_plugins/gazebo_attachment.h
+++ include/pal_gazebo_plugins/gazebo_attachment.h
@@ -1,10 +1,11 @@
 /*
-  @file
-
-  @author victor
-
-  @copyright (c) 2018 PAL Robotics SL. All Rights Reserved
-*/
+ * Copyright 2019 PAL Robotics SL. All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited,
+ * unless it was supplied under the terms of a license agreement or
+ * nondisclosure agreement with PAL Robotics SL. In this case it may not be
+ * copied or disclosed except in accordance with the terms of that agreement.
+ */
 #ifndef GAZEBO_ATTACHMENT_H
 #define GAZEBO_ATTACHMENT_H
 
--- include/pal_gazebo_plugins/gazebo_wifi_ap.h
+++ include/pal_gazebo_plugins/gazebo_wifi_ap.h
@@ -1,39 +1,11 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2014, PAL Robotics S.L.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// * Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-// * Neither the name of PAL Robotics S.L. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//////////////////////////////////////////////////////////////////////////////
-
 /*
- *  gazebo_wifi_ap.cpp
- *  Copyright (c) 2014 PAL Robotics sl. All Rights Reserved
- *  Created on: 5/19/2014
- *      Author: luca
- * \brief A plugin for gazebo for simulating wifi signal emitted by wifi access points
- * \author  Luca Marchionni (luca.marchionni@pal-robotics.com)
+ * Copyright 2019 PAL Robotics SL. All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited,
+ * unless it was supplied under the terms of a license agreement or
+ * nondisclosure agreement with PAL Robotics SL. In this case it may not be
+ * copied or disclosed except in accordance with the terms of that agreement.
  */
-
 #ifndef GAZEBO_WIFI_AP_CPP
 #define GAZEBO_WIFI_AP_CPP
 
